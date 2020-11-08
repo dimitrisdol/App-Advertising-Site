8437dbbabd4f180827d9a259f345b8aaec9833a6
jimmisD <jimmdologlou@gmail.com>
2020-11-08 22:49:39 +0200

    Update 1.1

--- Akou site/script.js
+++ Akou site/script.js
@@ -2,8 +2,6 @@ const navbar = document.querySelector(".navbar");
 const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
-const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
 
 window.addEventListener("scroll", () => {
   mainFn();
@@ -32,7 +30,7 @@ const mainFn = () => {
         progressBarPercents[i];
     });
   }
-};
+}; 
 
 mainFn();
 
